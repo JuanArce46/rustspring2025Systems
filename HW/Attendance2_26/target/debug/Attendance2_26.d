674c8719a4183839b97b9abfd8d0a41f4c6b2d46
JuanArce46 <157346141+JuanArce46@users.noreply.github.com>
2025-02-27 03:09:29 +0000

    attendance verification

---
+++ HW/Attendance2_26/target/debug/Attendance2_26.d
@@ -0,0 +1,1 @@
+/workspaces/rustspring2025Systems/HW/Attendance2_26/target/debug/Attendance2_26: /workspaces/rustspring2025Systems/HW/Attendance2_26/src/main.rs
---
+++ HW/Attendance2_26/target/debug/deps/Attendance2_26-d430be5829b79c66.d
@@ -0,0 +1,5 @@
+/workspaces/rustspring2025Systems/HW/Attendance2_26/target/debug/deps/Attendance2_26-d430be5829b79c66: src/main.rs
+
+/workspaces/rustspring2025Systems/HW/Attendance2_26/target/debug/deps/Attendance2_26-d430be5829b79c66.d: src/main.rs
+
+src/main.rs:
